c87ae6ba621697b10e1e8ffa44300350724238c1
Vitaliy Bulyma <vitaliy_bulyma@yahoo.ca>
2019-10-06 17:07:33 -0400

    Add project files.

---
+++ Raffle_Bundle.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Raffle_Bundle.aspx.cs" Inherits="Raffle_Bundle.Raffle_Bundle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ruffle Tool</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Raffle Tickets <br/> Purchasing and Bundling App</h1>
+            <label>Enter a number of tickets you would like to buy: <br/> </label>
+            <asp:TextBox runat="server" ID="user_input"></asp:TextBox>
+            <%--<asp:RequiredFieldValidator runat="server"
+                ControlToValidate="user_input"
+                ErrorMessage="Enter a minimum of 1 ticket"></asp:RequiredFieldValidator>--%>
+            <%--Validator from bdukesbdukes. “Asp.net Validation to Make Sure Textbox Has Integer Values.” Stack Overflow, stackoverflow.com/questions/1427629/asp-net-validation-to-make-sure-textbox-has-integer-values.--%>
+            <%--Cannot find how to have no MaxValue --%>
+            <asp:RangeValidator runat="server"
+                ControlToValidate="user_input"
+                ErrorMessage="Minimum number of tickets is 1."
+                MaximumValue="1000000" 
+                MinimumValue="1"
+                Type="Integer"></asp:RangeValidator>
+
+        </div>
+        <div>
+            <asp:Button runat="server" Text="Submit" ID="submit_button" />
+        </div>
+        <div runat="server" id="bundles_summary"></div>
+    </form>
+</body>
+</html>
---
+++ Raffle_Bundle.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Raffle_Bundle
+{
+    public partial class Raffle_Bundle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                Page.Validate();
+                if (Page.IsValid)
+                {
+                    int User_Input = Convert.ToInt32(user_input.Text);
+                    bundles_summary.InnerHtml = "";
+                    if (User_Input < 51) //0-50 Tickets, bundled Individually
+                    {
+                        double Total_cost = ((User_Input) * (0.25));
+                            bundles_summary.InnerHtml += "Number of ticket(s) ordered: " + User_Input + "<br>";
+                        for (int i=1; i < 51; i++)
+                        {
+                            bundles_summary.InnerHtml += "You received a bundle of 1! That's "+i+" tickets"+"<br>";
+                        }
+                            bundles_summary.InnerHtml += "Your total ticket(s) is " + User_Input + " and your cost is $ "+Total_cost+"cad"+"<br>";
+                    }//end of 0-50
+                    
+                    
+                    if (User_Input >50&&User_Input<151)//51 to 150 tickets, bundled in sets of 2
+                    {
+                        double Total_cost = ((User_Input) * (0.25));
+                            bundles_summary.InnerHtml += "Number of ticket(s) ordered: " + User_Input + "<br>";
+                        //checking if the are any "leftovers" using modulus
+                        if (User_Input % 2 == 0) 
+                        { 
+                        for (int count = 2; count < User_Input+2; count=count+2)
+                        {
+                            bundles_summary.InnerHtml += "You received a bundle of 2! That's " + count + " tickets!" + "<br>";
+                        }
+                        }
+                        else //if there are "leftovers" (User_Input % 2 != 0), running regular count, printing the remainder
+                        {
+                            for (int count = 2; count < User_Input; count = count + 2)
+                            {
+                                bundles_summary.InnerHtml += "You received a bundle of 2! That's " + count + " tickets!" + "<br>";
+                            }
+                            int Remainder_mod2 = User_Input % 2;
+                            bundles_summary.InnerHtml += "Your leftover is " + Remainder_mod2 + " ticket(s). Thats " + User_Input+" ticket(s)!"+"<br>";
+                        }
+                        bundles_summary.InnerHtml += "Your total ticket(s) is " + User_Input + " and your cost is $ " + Total_cost + "cad!" + "<br>";
+                    }//end of 50-150
+                    
+                    
+                    if (User_Input > 150 && User_Input < 301)//150 to 300 tickets, bundled in sets of 3
+                    {
+                        double Total_cost = ((User_Input) * (0.25));
+                            bundles_summary.InnerHtml += "Number of ticket(s) ordered: " + User_Input + "<br>";
+                        if (User_Input % 3 == 0)
+                            for (int count = 3; count < User_Input+3; count = count + 3)
+                        {
+                            bundles_summary.InnerHtml += "You received a bundle of 3! That's " + count + " ticket(s)!" + "<br>";
+                        }
+                        else 
+                        {
+                            for (int count = 3; count < User_Input; count = count + 3)
+                            {
+                                bundles_summary.InnerHtml += "You received a bundle of 3! That's " + count + " ticket(s)!" + "<br>";
+                            }
+                            int Remainder_mod3 = User_Input % 3;//checking if the are any "leftovers" using modulus
+                            bundles_summary.InnerHtml += "Your leftover is " + Remainder_mod3 + " ticket(s). Thats " + User_Input + " ticket(s)!" + "<br>";
+                        }
+                        bundles_summary.InnerHtml += "Your total ticket(s) is " + User_Input + " and your cost is $ " + Total_cost + "cad!" + "<br>";
+                    }//end of 150 - 300
+                    
+                    
+                    if (User_Input > 300)//more then 300 tickets, bundled by 5
+                    {
+                        double Total_cost = ((User_Input) * (0.25));
+                            bundles_summary.InnerHtml += "Number of ticket(s) ordered: " + User_Input + "<br>";
+                        if (User_Input % 5 == 0)
+                            for (int count = 5; count < User_Input+5; count = count + 5)
+                        {
+                            bundles_summary.InnerHtml += "You received a bundle of 5! That's " + count + " tickets" + "<br>";
+                        }
+                        else 
+                        {
+                            for (int count = 5; count < User_Input; count = count + 5)
+                            {
+                                bundles_summary.InnerHtml += "You received a bundle of 5! That's " + count + " tickets" + "<br>";
+                            }
+                            int Remainder_mod5 = User_Input % 5;//checking if the are any "leftovers" using modulus
+                            bundles_summary.InnerHtml += "Your leftover is " + Remainder_mod5 + " ticket(s). Thats " + User_Input + " ticket(s)!" + "<br>";
+                        }
+                        bundles_summary.InnerHtml += "Your total ticket(s) is " + User_Input + " and your cost is $ " + Total_cost + "cad!" + "<br>";
+                    }//end off 300+
+
+
+
+                }
+            }
+        }
+    }
+}
